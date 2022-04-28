1340e10db76af373b06663aad8bdbc916cfe6147
Shifan Zhang <szhang659@wisc.edu>
2022-04-28 18:18:15 -0500

    Latest version.

--- proj.R
+++ proj.R
@@ -1,14 +1,23 @@
-if (!require("tidyverse")) {
-  install.packages("tidyverse")
-  stopifnot(require("tidyverse"))
+rm(list=ls())
+
+RpackagesDir="R/library"
+
+args = (commandArgs(trailingOnly=TRUE))
+if(length(args) == 1){
+  files = args
+} else {
+  cat('usage: Rscript projR.R <files>\n', file=stderr())
+  stop()
 }
 
-if (!require("tree")) {
-  install.packages("tree")
-  stopifnot(require("tree"))
-}
+# Tell R to search in RpackagesDir, in addition to where it already
+# was searching, for installed R packages.
+.libPaths(new=c(RpackagesDir, .libPaths()))
 
-d = read_csv('merged09Q1.csv') # might need to change this (how to read file in parallel)
+require("tree", lib=RpackagesDir)
+require("tidyverse", lib=RpackagesDir)
+
+d = read_csv(files) # might need to change this (how to read file in parallel)
 
 df = d %>%  #might need to change this, depends on how many columns and column name actually in the slurm bin bash file 
   mutate(total_time = difftime(Trip_Dropoff_DateTime, Trip_Pickup_DateTime, units = "mins")) %>% 
@@ -18,15 +27,15 @@ df = d %>%  #might need to change this, depends on how many columns and column n
                                   Hour >= 12 & Hour<=16 ~ "afternoon",
                                   Hour >= 16 & Hour<=24 ~ "evening",
                                   Hour >= 0 & Hour <=4 ~ "midnight")) %>% 
-  select(-c(Date, Time, Hour, Trip_Dropoff_DateTime)) %>% 
-  mutate(Part_of_Date = as.factor(Part_of_Date)) %>% 
-  mutate(total_time = as.numeric(total_time))
+  select(-c(Date, Time, Hour, Payment_Type, Trip_Dropoff_DateTime)) %>% 
+  mutate(Part_of_Date = as.factor(Part_of_Date))
+
 
 df = na.omit(df)
 
 tree1 = tree(Total_Amt ~., data = df) #build regression tree
 
-png("tree.png", width = 600, height = 600) #might need to change name since parallel computing, also don't know whether slurm support png.
+png(paste(files,".png",sep=""), width = 600, height = 600) #might need to change name since parallel computing, also don't know whether slurm support png.
 # 2. Create the plot
 plot(tree1)
 text(tree1, pretty = 0, cex = 0.8)
